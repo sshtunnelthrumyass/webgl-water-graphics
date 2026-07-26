75d814f6b6880f7561b465e7a98da7a606c6ea65
Michael Price <271989644+sshtunnelthrumyass@users.noreply.github.com>
2026-07-27 00:34:26 +0100

    swagger

--- main.js
+++ main.js
@@ -45,24 +45,18 @@ var gravity;
 var radius;
 var paused = false;
 
-window.onload = function() {
-  var ratio = window.devicePixelRatio || 1;
-  var help = document.getElementById('help');
-
-  function onresize() {
-    var width = innerWidth - help.clientWidth - 20;
+function onresize() {
+    var width = innerWidth;
     var height = innerHeight;
+
     gl.canvas.width = width * ratio;
     gl.canvas.height = height * ratio;
-    gl.canvas.style.width = width + 'px';
-    gl.canvas.style.height = height + 'px';
+
+    gl.canvas.style.width = width + "px";
+    gl.canvas.style.height = height + "px";
+
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.matrixMode(gl.PROJECTION);
-    gl.loadIdentity();
-    gl.perspective(45, gl.canvas.width / gl.canvas.height, 0.01, 100);
-    gl.matrixMode(gl.MODELVIEW);
-    draw();
-  }
+}
 
   document.body.appendChild(gl.canvas);
   gl.clearColor(0, 0, 0, 0);
